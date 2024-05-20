3f1439c5ea69aebe4d211d2c1e75a625411bb611
dgush <dgushch@gmail.com>
2024-05-20 13:21:56 +0400

    0.4.4

--- init.py
+++ init.py
@@ -7,6 +7,8 @@ from datetime import datetime
 
 import traceback
 import logging
+import os
+import re
 
 from dotenv import load_dotenv
 from os import getenv
@@ -47,9 +49,27 @@ async def set_main_menu() -> None:
     await bot.set_my_commands (main_menu_commands)
 
 
-def log_error(message):
-    timestamp = datetime.now (TZ)
-    filename = traceback.format_exc () [1]
-    line_number = traceback.format_exc () [2]
-    logging.error (f'{timestamp} {filename} {line_number}: {message}')
+def log_error(message, with_traceback: bool = True):
+    now = datetime.now(TZ)
+    log_folder = now.strftime ('%m-%Y')
+    log_path = os.path.join('logs', log_folder)
+
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    log_file_path = os.path.join(log_path, f'{now.day}.log')
+    logging.basicConfig (level=logging.WARNING, filename=log_file_path, encoding='utf-8')
+    if with_traceback:
+        ex_traceback = traceback.format_exc()
+        tb = ''
+        start_row = '  File "C:' if DEBUG else '  File'
+        tb_split = ex_traceback.split('\n')
+        for row in tb_split:
+            if row.startswith(start_row) and not re.search ('venv', row):
+                tb += f'{row}\n'
+
+        msg = ex_traceback.split ('\n\n') [-1]
+        logging.warning(f'{now}\n{tb}\n{msg}\n---------------------------------\n')
+    else:
+        logging.warning(f'{now}\n{message}\n\n---------------------------------\n')
 
